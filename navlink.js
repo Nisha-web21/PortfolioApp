d1785b4d49d852f6d8db98dd95124545477f2387
Nisha <79060885+Nisha-web21@users.noreply.github.com>
2021-12-05 15:17:26 -0700

    Add files via upload

---
+++ navlink.js
@@ -0,0 +1,22 @@
+export const navlink=[
+    {
+    name:"HOME",
+    id:"#home"
+},
+    {
+    name:"ABOUT ME",
+    id:"#about-me"
+},
+    {
+    name:"TECHNOLOGIES",
+    id:"#technologies"
+},
+    {
+    name:"PORTFOLIO",
+    id:"#portfolio"
+},
+    {
+    name:"CONTACT",
+    id:"#contact"
+}
+]
